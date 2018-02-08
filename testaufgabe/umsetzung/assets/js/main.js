d150a71df935752272ef5c8e3236028b30590ca2
Marcus Kopp <marcus.kopp86@gmail.com>
2018-02-08 15:42:01 +0100

    fixed the problem with hidden margins

--- testaufgabe/umsetzung/assets/js/main.js
+++ testaufgabe/umsetzung/assets/js/main.js
@@ -1,23 +1,21 @@
 function openTab(evt, tabName) {
     // Declare all variables
-    var i, tabcontent, tablinks;
+    var i, tabContents, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
-    newsItems = document.getElementsByClassName("news-item");
-    for (i = 0; i < newsItems.length; i++) {
-        newsItems[i].style.display = "none";
+    tabContents = document.getElementsByClassName("content-wrapper");
+    for (i = 0; i < tabContents.length; i++) {
+        tabContents[i].style.display = "none";
     }
 
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
-        tablinks[i].className = tablinks[i].className.replace(" inactive", "");
-        tablinks[i].className += " inactive";
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className = evt.currentTarget.className.replace(" inactive", " active");
+    evt.currentTarget.className += " active";
 
 }
